6ba16fa68533aaaa8b2682b75517c909b3b37f22
Malcolm Sparks <malcolm@juxt.pro>
2016-11-13 18:26:27 +0000

    Differentiate input and output context spec

--- src/yada/context.clj
+++ src/yada/context.clj
@@ -3,17 +3,33 @@
 (ns yada.context
   (:require
    [clojure.spec :as s]
+   clojure.string
+   yada.resource
    [ring.core.spec :as rs]))
 
 (defrecord Context [])
 
 (s/def :yada/context
-  (s/keys :req [:ring/request]))
+  (s/keys :req [:ring/request
+                :yada/method-token]))
 
 (defn context [model]
-  (when-not (s/valid? :yada/context model)
-    (throw
-     (ex-info
-      (format "Context model is not valid: %s" (s/explain-str :yada/context model))
-      {:model model :explain (s/explain-data :yada/context model)})))
-  (map->Context model))
+  (let [input-spec (s/keys :req [:ring/request])]
+    (when-not (s/valid? input-spec model)
+      (throw
+       (ex-info
+        (format "Input context model is not valid: %s"
+                (s/explain-str input-spec model))
+        {:model model :explain (s/explain-data input-spec model)}))))
+
+  (let [augmented-model
+        (-> model
+            (assoc :yada/method-token (-> model :ring/request :request-method name clojure.string/upper-case))
+            )]
+    (when-not (s/valid? :yada/context augmented-model)
+      (throw
+       (ex-info
+        (format "Augmented context model is not value: %s"
+                (s/explain-str :yada/context augmented-model))
+        {:model augmented-model :explain (s/explain-data :yada/context augmented-model)})))
+    (map->Context augmented-model)))
