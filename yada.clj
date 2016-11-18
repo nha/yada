1a0ad0e91ba2bd1120b042186513f0b9632aa0d4
Malcolm Sparks <malcolm@juxt.pro>
2016-11-18 11:50:25 +0000

    yada API ns added to 2.0

--- yada.clj
+++ yada.clj
@@ -1,3 +1,11 @@
 ;; Copyright © 2014-2016, JUXT LTD.
 
-(ns yada.yada)
+(ns yada.yada
+  (:require
+   yada.context
+   yada.handler
+   [potemkin :refer (import-vars)]))
+
+(import-vars
+ [yada.handler handler accept-request]
+ [yada.resource resource])
